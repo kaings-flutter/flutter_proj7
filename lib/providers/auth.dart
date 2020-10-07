bcb3ca7a46dcf51ba8f03cf1dcd072283f6d7ca5
willys_tjiawi <willys@huubap.com>
2020-10-07 18:23:40 +0800

    refactor & handle authentication error

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -2,37 +2,47 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import '../secret/my_credentials.dart';
+import '../models/http_exception.dart';
 
 class Auth with ChangeNotifier {
   String _token;
   DateTime _expiryDate;
   String _userId;
 
+  Future<void> _authenticate(
+      String email, String password, String authCategory) async {
+    final url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:$authCategory?key=${MyCredentials.PROJ6_API_KEY}';
+
+    try {
+      final response = await http.post(url,
+          body: json.encode({
+            'email': email,
+            'password': password,
+            'returnSecureToken': true,
+          }));
+
+      print('$authCategory response..... ${json.decode(response.body)}');
+
+      final responseData = json.decode(response.body);
+
+      // we create custom error handling because firebase does not throw error
+      // in case there is an error, the error will show in the response as follows
+      if (responseData['error'] != null) {
+        throw HttpException(responseData['error']['message']);
+      }
+    } catch (err) {
+      print('_authenticate_err..... $err');
+
+      throw err;
+    }
+  }
+
   Future<void> signUp(String email, String password) async {
-    const url =
-        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${MyCredentials.PROJ6_API_KEY}';
-
-    final response = await http.post(url,
-        body: json.encode({
-          'email': email,
-          'password': password,
-          'returnSecureToken': true,
-        }));
-
-    print('signUp_response..... ${json.decode(response.body)}');
+    return _authenticate(email, password, 'signUp');
   }
 
   Future<void> signIn(String email, String password) async {
-    const url =
-        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${MyCredentials.PROJ6_API_KEY}';
-
-    final response = await http.post(url,
-        body: json.encode({
-          'email': email,
-          'password': password,
-          'returnSecureToken': true,
-        }));
-
-    print('signIn_response..... ${json.decode(response.body)}');
+    return _authenticate(email, password, 'signInWithPassword');
   }
 }
--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -2,6 +2,8 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+
+import '../models/http_exception.dart';
 import '../providers/auth.dart';
 
 enum AuthMode { Signup, Login }
@@ -101,6 +103,25 @@ class _AuthCardState extends State<AuthCard> {
   var _isLoading = false;
   final _passwordController = TextEditingController();
 
+  void _showErrorDialog(String errMessage) {
+    // `context` is available EVERYWHERE in the State class!!!!!
+    showDialog(
+      context: context,
+      builder: (ctx) => AlertDialog(
+        title: Text('An Error Occurred!'),
+        content: Text(errMessage),
+        actions: [
+          FlatButton(
+            child: Text('OK'),
+            onPressed: () {
+              Navigator.of(ctx).pop();
+            },
+          )
+        ],
+      ),
+    );
+  }
+
   void _submit() async {
     if (!_formKey.currentState.validate()) {
       // Invalid!
@@ -110,19 +131,44 @@ class _AuthCardState extends State<AuthCard> {
     setState(() {
       _isLoading = true;
     });
-    if (_authMode == AuthMode.Login) {
-      // Log user in
-      await Provider.of<Auth>(context, listen: false).signIn(
-        _authData['email'],
-        _authData['password'],
-      );
-    } else {
-      // Sign user up
-      await Provider.of<Auth>(context, listen: false).signUp(
-        _authData['email'],
-        _authData['password'],
-      );
+
+    try {
+      if (_authMode == AuthMode.Login) {
+        // Log user in
+        await Provider.of<Auth>(context, listen: false).signIn(
+          _authData['email'],
+          _authData['password'],
+        );
+      } else {
+        // Sign user up
+        await Provider.of<Auth>(context, listen: false).signUp(
+          _authData['email'],
+          _authData['password'],
+        );
+      }
+    } on HttpException catch (err) {
+      // ONLY catch custom HttpException error
+      var errorMessage = 'Authentication Failed! ';
+      if (err.toString().contains('EMAIL_EXISTS')) {
+        errorMessage += 'The email address is already in use.';
+      } else if (err.toString().contains('INVALID_EMAIL')) {
+        errorMessage += 'This is not a valid email address.';
+      } else if (err.toString().contains('WEAK_PASSWORD')) {
+        errorMessage += 'This password is too weak.';
+      } else if (err.toString().contains('EMAIL_NOT_FOUND')) {
+        errorMessage += 'Could not find a user with that email.';
+      } else if (err.toString().contains('INVALID_PASSWORD')) {
+        errorMessage += 'Invalid password.';
+      }
+
+      _showErrorDialog(errorMessage);
+    } catch (err) {
+      // catch any other errors
+      const errorMessage = 'Authentication failed! Please try again later!';
+
+      _showErrorDialog(errorMessage);
     }
+
     setState(() {
       _isLoading = false;
     });
